eb9f6569ef1da0c79821aad14845cb0d78102386
Piyush Tiwari <piyushtiwari919921@gmail.com>
2026-07-18 21:14:17 +0530

    refactor(server): enhance error handling in getUserInfo controller

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -5,14 +5,16 @@ const getUserInfo = async (req, res) => {
       return res.status(500).json({ message: "No username present" });
     }
     const url = `https://api.github.com/users/${username}`;
+
     const response = await fetch(url);
-    // console.log(response);
 
     if (!response.ok) {
       if (response.status === 404) {
-        throw new Error("User not found.");
+        return res.status(404).json({ message: "User not found." });
       } else {
-        throw new Error(`API Error: ${response.statusText}`);
+        return res
+          .status(400)
+          .json({ message: `API Error: ${response.statusText}` });
       }
     }
     const data = await response.json();
@@ -22,9 +24,7 @@ const getUserInfo = async (req, res) => {
     return res.status(200).json({ data: data });
   } catch (err: any) {
     console.error(err);
-    return res
-      .status(500)
-      .json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   }
 };
 
