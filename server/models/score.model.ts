a112791a9d15980a13556432cf7979d3c2302346
Piyush Tiwari <piyushtiwari919921@gmail.com>
2026-08-30 22:41:33 +0530

    fix(score): add missing reference to User model in score schema

--- server/models/score.model.ts
+++ server/models/score.model.ts
@@ -1,9 +1,11 @@
 import { Schema, model } from "mongoose";
+import User from "./user.model.js";
 const scoreSchema = new Schema(
   {
     githubUsername: {
       type: String,
       required: true,
+      ref:User
     },
     scores: {
       type: [Number],
